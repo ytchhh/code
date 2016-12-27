fc1f715cb0a7d357c4bfb045bea15b89f04511b9
yangtingcheng <917247978@qq.com>
2016-12-27 19:53:03 +0800

    second change

--- STLSource/Test11_10STL/stl_construct.h
+++
@@ -1,5 +0,0 @@
-template <class T1, class T2>
-inline void construct(T1* p, const T2& value) 
-{
-  new (p) T1(value);
-}
--- STLSource/Test11_10STL/stl_iterator.h
+++
@@ -1,8 +0,0 @@
-#ifndef _STL_ITERATOR_H
-#define _STL_ITERATOR_H
-
-template <class T>
-inline T* value_type(const T*) 
-{ return (T*)(0); }
-
-#endif
